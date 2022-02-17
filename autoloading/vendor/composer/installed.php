65f3fbdbbcd4112d99ea9ef5335d155de562d759
sergiosalguero <salgueroflor1982@gmail.com>
2022-02-17 19:17:03 +0100

    symfony

---
+++ autoloading/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '548995b37eec3ca0b2735adba6f50429044aa50b',
+        'name' => 'sergiosalguero/autoloading',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sergiosalguero/autoloading' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '548995b37eec3ca0b2735adba6f50429044aa50b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
